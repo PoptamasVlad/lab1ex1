acdc08f85f66f175db9f6202621f09817440202b
Poptamas Vlad <poptamasvlad@gmail.com>
2022-11-25 20:12:14 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+            paralelipiped dreptunghic, va afisa volumul lui*/
+
+            int lungime = int.Parse(Console.ReadLine());
+            int latime = int.Parse(Console.ReadLine());
+            int inaltime = int.Parse(Console.ReadLine());
+
+            int volum = lungime * latime * inaltime;
+            Console.WriteLine(volum);
+        }
+    }
+}
